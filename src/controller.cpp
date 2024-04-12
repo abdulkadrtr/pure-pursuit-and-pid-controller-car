1eab3511d843669e347811a00d4d7de4fe6c7035
Abdulkadir TÜRE <87595266+abdulkadrtr@users.noreply.github.com>
2024-04-12 11:48:51 +0300

    error-abs-add

--- src/controller.cpp
+++ src/controller.cpp
@@ -168,6 +168,7 @@ namespace controller
     double longitudinal_velocity_error = 0.0;
     // Calculate the longitudinal velocity error here.
     longitudinal_velocity_error = target_speed - odometry_->twist.twist.linear.x; // Hesaplanan longitudinal velocity error
+    longitudinal_velocity_error = std::abs(longitudinal_velocity_error);
     return longitudinal_velocity_error;
   }
 
